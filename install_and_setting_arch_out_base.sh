e2c648763e79abe7903c4b63fbae115a77696b9a
vkluad <vkluad@gmail.com>
2021-01-05 22:34:06 +0200

    add user

--- install_and_setting_arch_out_base.sh
+++ install_and_setting_arch_out_base.sh
@@ -5,6 +5,12 @@ read -p "Ether the host name: " HOSTNAME
 read -p "Enter user name: " USERNAME
 
 echo $HOSTNAME > /etc/hostname
+echo "Add user $USERNAME"
+useradd -m -g users -G wheel -s /bin/bash $USERNAME
+echo "Password for user $USERNAME"
+passwd $USERNAME
+echo "Password for root user"
+passwd
 
 echo 'Added locale in locale.gen'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
@@ -21,3 +27,10 @@ echo "127.0.0.1  localhost
 ::1        localhost ip6-localhost ip6-loopback
 ff02::1    ip6-allnodes
 ff02::2    ip6-allrouters" >> /etc/hosts
+
+echo 'Recommented library for 32-bit apps'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+pacman -Syy
+
+pacman -S dialog wpa_supplicant
