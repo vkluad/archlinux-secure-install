9df910d732d50e7673690c0515e4f0cd524b0078
vkluad <vkluad@gmail.com>
2021-01-07 13:22:11 +0200

    update

--- install_arch.sh
+++ install_arch.sh
@@ -175,7 +175,7 @@ reflector --verbose --country 'Ukraine' --sort rate --save /etc/pacman.d/mirrorl
 sleep 2;
 
 echo "Install base package"
-pacstrap /mnt base base-devel linux linux-firmware nano netctl dhcpcd intel-ucode zsh reflector f2fs-tools sudo
+pacstrap /mnt base base-devel linux linux-firmware nano netctl dhcpcd intel-ucode zsh reflector f2fs-tools
 sleep 2;
 
 echo "Copying mirrorlist"
