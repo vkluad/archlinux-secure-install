eda3a1dd32c90a1fb2fc9eeb5cac7cc118198588
vkluad <vkluad@gmail.com>
2021-02-15 18:14:50 +0200

    temp

--- programm_install.sh
+++ programm_install.sh
@@ -4,8 +4,7 @@ firefox blender cuda libreoffice-still discord telegram-desktop vulkan-devel nvi
 lib32-nvidia-cg-toolkit chromium tor ghex handbrake htop jdk11-openjdk jre-openjdk \
 jre-openjdk-headless jre11-openjdk jre11-openjdk-headless shotcut efibootmgr embree \
 exfat-utils gstreamer-vaapi iotop screen neofetch zerotier-one ncdu ntfs-3g rsync \
-chrome-gnome-shell torbrowser-launcher qbittorrent obs-studio clevis tpm2-tools \
-tpm2-tss tpm2-totp tpm2-abrmd dracut libpwquality Luksmeta Nmap
+chrome-gnome-shell torbrowser-launcher qbittorrent obs-studio 
 
 
 cd /tmp
@@ -17,7 +16,14 @@ yay -S wd719x-firmware aic94xx-firmware upd72020x-fw
 yay -S optimus-manager gdm-prime libgdm-prime zoom teams skypeforlinux-stable-bin youtube-music-appimage\
 clion clion-jre clion-cmake clion-gdb clion-lldb plymouth
 
+
+# configure some programs
+
 systemctl enable gdm
 systemctl enable fstrim.timer
 
 cp -r ./configuration/optimus-manager.conf /etc/optimus-manager/optimus-manager.conf
+
+# configure pulse audio
+sudo echo "load-module module-echo-cancel source_name=noechosource sink_name=noechosink
+set-default-source noechosource">>/etc/pulse/default.pa # removes noise from the micro
